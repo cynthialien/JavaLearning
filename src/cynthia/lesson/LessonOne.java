5e9d1536facc3bf4a20d1f88a848d22e1a965b2f
Cynthia <cynthia80121@yahoo.com.tw>
2019-09-13 17:27:15 +0800

    Init project

---
+++ src/cynthia/lesson/LessonOne.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cynthia.lesson;
+
+/**
+ * @author Cynthia
+ *
+ */
+public class LessonOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
